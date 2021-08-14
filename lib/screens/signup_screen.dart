83024b2b00a0e06b3f07ff2621b02de1d48c2da7
bxedy <joaov.martins2002@gmail.com>
2021-08-14 18:36:58 -0300

    Adding the login and signup screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,9 +1,8 @@
 import 'package:auto_journal/constants.dart';
-import 'package:auto_journal/screens/signup_screen.dart';
 import 'package:auto_journal/widgets/dark_card.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,35 +30,8 @@ class LoginScreen extends StatelessWidget {
                     height: 20,
                   ),
                   DarkCard(
-                    text: 'Login',
+                    text: 'Signup',
                     onTap: () {},
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('New to AutoJornal ?'),
-                      SizedBox(width: 5.0),
-                      InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => Signup(),
-                            ),
-                          );
-                        },
-                        child: Text(
-                          'Register',
-                          style: TextStyle(
-                            color: Colors.black26,
-                            fontFamily: 'Trueno',
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      )
-                    ],
                   ),
                 ],
               ),
